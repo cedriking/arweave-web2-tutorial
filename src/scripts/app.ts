7c4d6a810f5479f6c826f3a91711876f72833b08
Cedrik <cedrik.git@tryninja.io>
2021-07-14 10:33:30 -0500

    ardb deploy

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -4,23 +4,46 @@ import "bootstrap";
 import * as $ from "jquery";
 import Arweave from 'arweave';
 import Account from "./account";
+import ArDB from '@textury/ardb';
 
 const arweave = Arweave.init({
-  host: 'localhost',
-  port: 1984,
-  protocol: 'http'
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https'
 });
+
+const ardb = new ArDB(arweave);
 
 const account = new Account();
 
 $(() => {
+  ardb.search('transactions').appName('TODO224466').find().then(async (txs) => {
+    const $list = $('#itemsList');
+
+    for(const tx of txs) {
+      const res = await arweave.api.get(`${tx.id}`);
+      $list.append(`<li>${res.data}</li>`);
+    }
+  });
+
   $('#itemAdd').on('submit', async e => {
     e.preventDefault();
 
     const $input = $('#inputItem');
     const $list = $('#itemsList');
 
-    $list.append(`<li>${$input.val()}</li>`);
+    const todo = $input.val().toString();
+
+    const tx = await arweave.createTransaction({
+      data: todo
+    });
+
+    tx.addTag('App-Name', 'TODO224466');
+
+    await arweave.transactions.sign(tx);
+    await arweave.transactions.post(tx);
+
+    $list.append(`<li>${todo}</li>`);
 
     $input.val('');
   });
